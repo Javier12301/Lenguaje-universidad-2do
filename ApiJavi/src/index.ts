ba01dae9a9692f169a395a718b948d945ab23719
Javier12301 <javierramirez1230123@gmail.com>
2022-09-01 09:47:17 -0300

    f

---
+++ ApiJavi/src/index.ts
@@ -0,0 +1,6 @@
+export const myfunction = () =>{
+console.log("Jelou wor");
+}
+
+
+
---
+++ ApiJavi/src/model/usuario.ts
@@ -0,0 +1,9 @@
+import { prop } from "@typegoose/typegoose";
+
+class KittenClass {
+  @prop()
+  public name?: string;
+}
+
+
+  
