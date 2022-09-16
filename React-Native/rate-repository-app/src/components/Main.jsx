f7d683895353f4a29ceeb0eb0ce34c66c2f64351
Javier12301 <javierramirez1230123@gmail.com>
2022-09-16 18:15:36 -0300

    React Add

---
+++ React-Native/rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Rate Repository Application</Text>
+        </View>
+    )
+}
